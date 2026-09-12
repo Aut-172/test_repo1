8af4b62d5a66a8fc92726b679238d3f035f4aadb
Aut-172 <1721892105@qq.com>
2026-09-12 15:35:19 +0800

    build: add Spring Boot Maven scaffold

---
+++ src/main/java/com/aut172/testrepo1/TestRepo1Application.java
@@ -0,0 +1,12 @@
+package com.aut172.testrepo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRepo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRepo1Application.class, args);
+    }
+}
---
+++ src/test/java/com/aut172/testrepo1/TestRepo1ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.aut172.testrepo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRepo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
